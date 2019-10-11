a695a18dd4d0a6733811015eb3824c9af1a1d711
Joao <joao.araujo2@br.ey.com>
2019-10-10 22:50:40 -0300

    bug fix and signup page is implemented!

--- projmocambique/patrimonio/views.py
+++ projmocambique/patrimonio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # from patrimonio.models import User
-from patrimonio.forms import User
+from patrimonio.forms import NewUserForm
 # Create your views here.
 
 #first page
