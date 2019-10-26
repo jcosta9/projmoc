d49acd86587604da5c900ce745303dc9b919212f
Joao <joao.araujo2@br.ey.com>
2019-10-25 22:09:21 -0300

    Criação de algumas poucas views: - About - Lista de Bens - Detalhe do bem

--- projmoc2/gest_patr/views.py
+++ projmoc2/gest_patr/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Bem
+from django.utils import timezone
+from .forms import BemForm
+
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class BemListView(ListView):
+    model = Bem
+
+    def get_queryset(self):
+        return Bem.objects.filter(dataPreenchimento__lte=timezone.now()).order_by('-dataPreenchimento')
+
+class BemDetailView(DetailView):
+    model = Bem
