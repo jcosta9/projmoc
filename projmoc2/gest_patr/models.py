5bd259822c07dd0cc2deef4b575b8ee8be1aa901
Joao <joao.araujo2@br.ey.com>
2019-10-25 21:57:03 -0300

    Criação da view dos bens que serão cadastrados

--- projmoc2/gest_patr/models.py
+++ projmoc2/gest_patr/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Bem(models.Model):
+    nome = models.CharField(max_length=200)
+    dataPreenchimento = models.DateField(default=timezone.now)
+    codClassificacaoGeral = models.DecimalField(max_digits=8, decimal_places=0)
+    preenchidoPor = models.ForeignKey('auth.User')
+
+    def get_absolute_url(self):
+        return reverse("bem_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.nome
