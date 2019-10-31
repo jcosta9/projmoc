5f851c29470a2911a95ace050a3564152114fd92
Joao Paulo Araujo <jpaulo.caraujo@gmail.com>
2019-10-30 23:12:08 -0300

    remove unnecessary files

--- projmocambique/patrimonio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from patrimonio.models import UserProfileInfo, User
-# Register your models here.
-# admin.site.register(User)
-admin.site.register(UserProfileInfo)
